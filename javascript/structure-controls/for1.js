affabda7bfd618b091c3fbc7ae2c98af4154b43f
Paulo Henrique <phenriq694@gmail.com>
2020-05-17 00:58:18 -0300

    New Document - javascript/structure-controls/for1.js

---
+++ javascript/structure-controls/for1.js
@@ -0,0 +1,13 @@
+for(let i = 1; i <= 10; i++) {
+  console.log(`i = ${i}`)
+}
+
+const testNotes = [6.7, 7.4, 9.8, 8.1, 7.7]
+for(let i = 0; i < testNotes.length; i++) {
+  console.log(`Test Note = ${testNotes[i]}`)
+}
+
+/**
+ * Source:
+ * Coder - Modern Development Cource - Section 4: Javascript: Structures Controls - Using the structure do for
+ */
