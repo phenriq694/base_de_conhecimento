4e5533b6042c635c54702996dd62454904a67dff
Paulo Henrique <phenriq694@gmail.com>
2020-05-20 03:58:09 -0300

    New Document - javascript/functions/params&returnOptinal.js

---
+++ javascript/functions/params&returnOptional.js
@@ -0,0 +1,20 @@
+function area(height, width) {
+  const area = height * width
+  if (area > 20) {
+    console.log(`Value above allowed: ${area}m2.`)
+  } else {
+    return area
+  }
+}
+
+// Output: 4
+console.log(area(2, 2))
+// Output: NaN
+console.log(area(2))
+// Output: NaN
+console.log(area())
+// Output: 6
+console.log(area(2, 3, 4, 5, 6))
+// Output: Value above allowed: 25m2.
+// Output: undefined
+console.log(area(5, 5))
